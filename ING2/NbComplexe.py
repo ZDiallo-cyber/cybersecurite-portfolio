ae5efe6d565af9d5042a5e641f0f9fa76b810874
Zakaria Diallo <zkdiallo68@gmail.com>
2026-09-05 21:32:06 +0200

    Remove misplaced files from repo

--- ING2/NbComplexe.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt,atan2,degrees
-
-a=float(input("Entre la partie réelle:"))
-b=float(input("Entre la partie imaginaire:"))
-module=sqrt((a**2)+(b**2))
-module=round(module,2)
-argument=degrees(atan2(b,a))
-argument=int(round(argument))
-print(f"Le module est {module} et l'argument est {argument} degrés")
